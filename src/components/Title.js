100234ae43b001843d4ecfc715516aaf349d76d2
Fabian Martinez <fabian.m.martinez@gmail.com>
2021-06-08 20:44:18 -0700

    Mauro - Add title component

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="title">
+            <h1>TheGram</h1>
+
+            <h2>Your Pics</h2>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis nihil blanditiis dolore similique officiis impedit repellat ab eligendi unde. Amet cupiditate vel asperiores veritatis quo ad libero fugit temporibus nesciunt?</p>
+        </div>
+    )
+}
